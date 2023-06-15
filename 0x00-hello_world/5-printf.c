6279079b2d7a3a2c71a113fd676da8fcc24ea59f
NoraOC <ihezueoluebubecynthia@gmail.com>
2023-06-15 15:49:40 -0700

    5-printf.c

---
+++ 0x00-hello_world/5-printf.c
@@ -0,0 +1,9 @@
+#include <stdio.h>
+/**
+ * main- use the function printf to output
+ * Return: 0 always
+ */
+int main(void)
+{
+printf("\"with proper grammar, but the outcome is a piece of art,");
+return (0);
