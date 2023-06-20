3859781cda30cec5f77f424e9b1769f1a131c683
NoraOC <ihezueoluebubecynthia@gmail.com>
2023-06-19 17:45:54 -0700

    6-size.c

--- 0x00-hello_world/6-size.c
+++ 0x00-hello_world/6-size.c
@@ -1,11 +1,11 @@
 #include <stdio.h>
 /**
- * main - A program that prints the size of various computer types
- * Return (0) (Success)
+ * main - Entry point
+ * Description: A program that prints the size of various computer types
+ * Return: Always 0(Success)
  */
 int main(void)
-{      
-       	char a;
+{      	char a;
 	int b;
 	long int c;
 	long long int d;
