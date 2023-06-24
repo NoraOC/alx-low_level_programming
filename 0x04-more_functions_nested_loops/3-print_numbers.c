07477e02a5e8e5058818ef348cb195bdb1ed09c8
NoraOC <ihezueoluebubecynthia@gmail.com>
2023-06-23 21:29:42 -0700

    5-more_numbers.c

--- 0x04-more_functions_nested_loops/3-print_numbers.c
+++ 0x04-more_functions_nested_loops/3-print_numbers.c
@@ -3,11 +3,12 @@
  * print_numbers - A function that prints the numbers, from 0 to 9
  * Return: Always 0
  */
+
 void print_numbers(void)
 {
 	char c;
 
-	for (c = '0'; c <= '0'; c++)
+	for (c = '0'; c <= '9'; c++)
 	{
 		_putchar(c);
 	}
