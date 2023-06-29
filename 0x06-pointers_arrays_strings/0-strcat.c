63f978a97f9675c596f9e75bd0a7b762b5338f52
NoraOC <ihezueoluebubecynthia@gmail.com>
2023-06-29 11:16:13 -0700

    0-strcat.c

--- 0x06-pointers_arrays_strings/0-strcat.c
+++ 0x06-pointers_arrays_strings/0-strcat.c
@@ -17,6 +17,9 @@ char *_strcat(char *dest, char *src)
 	}
 	for (z = 0; src[z] != '\0'; z++, length_of_string++)
 	{
+		dest[length_of_string] = src[j];
+	}
+	{
 		dest[length_of_string] = '\0';
 		return (dest);
 	}
