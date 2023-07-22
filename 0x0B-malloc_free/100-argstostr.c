432828b3b8474382a58fc21282dc0f75b521c079
NoraOC <ihezueoluebubecynthia@gmail.com>
2023-07-21 22:55:55 -0700

    100-argstostr.c

--- 0x0B-malloc_free/100-argstostr.c
+++ 0x0B-malloc_free/100-argstostr.c
@@ -10,26 +10,31 @@ char *argstostr(int ac, char **av)
 {
 	int i, n, r = 0, l = 0;
 	char *str;
-	
+
 	if (ac == 0 || av == NULL)
 		return (NULL);
+
 	for (i = 0; i < ac; i++)
 	{
-	for (n = 0; av[i][n]; n++)
-		l++;
+		for (n = 0; av[i][n]; n++)
+			l++;
 	}
 	l += ac;
+
 	str = malloc(sizeof(char) * l + 1);
 	if (str == NULL)
 		return (NULL);
 	for (i = 0; i < ac; i++)
 	{
-	for (str[r] = av[i][n];)
-	r++;
-	}
-	(str[r] == '\0';
+	for (n = 0; av[i][n]; n++)
 	{
-	for (str[r++] = '\n';
+		str[r] = av[i][n];
+		r++;
 	}
-	return (0);
-
+	if (str[r] == '\0')
+	{
+		str[r++] = '\n';
+	}
+	}
+	return (str);
+}
